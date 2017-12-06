f3ad3b1644eb5f071e0837cf73007f33cfc2b455
Guillaume <guillaumemas.gm@gmail.com>
2017-12-06 15:40:08 +0100

    Sport & user

--- app/src/test/java/classes/src/Sport.java
+++ app/src/test/java/classes/src/Sport.java
@@ -4,12 +4,28 @@ package classes.src;
  * Created by Romain on 28/11/2017.
  */
 
-public class Sport extends Utilisateur {
+public class Sport extends Activite {
 
     private int niveau = 0;
 
-    public Sport(int n) {
-        niveau = n;
+    public Sport(String lieu, String type_activite, int[] date, int[] horaire, int[] duree, int nb_place, int niveau) {
+        super(lieu, type_activite, date, horaire, duree, nb_place);
+        this.niveau = niveau;
+    }
+
+    public Sport(String lieu, String type_activite, int[] date, int[] horaire, int niveau) {
+        super(lieu, type_activite, date, horaire);
+        this.niveau = niveau;
+    }
+
+    public Sport(String lieu, String type_activite, int[] date, int[] horaire, int[] duree, int niveau) {
+        super(lieu, type_activite, date, horaire, duree);
+        this.niveau = niveau;
+    }
+
+    public Sport(String lieu, String type_activite, int[] date, int[] horaire, int nb_place, int niveau) {
+        super(lieu, type_activite, date, horaire, nb_place);
+        this.niveau = niveau;
     }
 
     public void setNiveau(int n) {
--- app/src/test/java/classes/src/Utilisateur.java
+++ app/src/test/java/classes/src/Utilisateur.java
@@ -5,4 +5,56 @@ package classes.src;
  */
 
 public class Utilisateur {
+    private String login;
+    private String mdp;
+    private String pseudo;
+    private String type_activités;
+    private String lieux;
+    private String commentaire;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getType_activités() {
+        return type_activités;
+    }
+
+    public void setType_activités(String type_activités) {
+        this.type_activités = type_activités;
+    }
+
+    public String getLieux() {
+        return lieux;
+    }
+
+    public void setLieux(String lieux) {
+        this.lieux = lieux;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public Utilisateur(String login, String mdp) {
+        this.login = login;
+        this.mdp = mdp;
+    }
+
 }
